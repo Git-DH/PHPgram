0b0a66cde6bc219fed8edf601df1257ceb896834
Git-DH <kdh9345@naver.com>
2022-06-29 18:23:43 +0900

    06-29

--- static/js/feed/index.js
+++ static/js/feed/index.js
@@ -5,12 +5,10 @@
         const body =  modal.querySelector('#id-modal-body');
         const frmElem = modal.querySelector('form');
         const btnClose = modal.querySelector('.btn-close');
-
         //이미지 값이 변하면
         frmElem.imgs.addEventListener('change', function(e) {
-            // form태그의 경우 .으로 자식에게 접근가능
+            console.log(`length: ${e.target.files.length}`);
             if(e.target.files.length > 0) {
-                // length값이 0보다 큰거면 파일선택을 했다는 뜻
                 body.innerHTML = `
                     <div>
                         <div class="d-flex flex-md-row">
@@ -29,9 +27,9 @@
 
                 const imgSource = e.target.files[0];
                 const reader = new FileReader();
-                reader.readAsDataURL(imgSource); //위치값
-                reader.onload = function() { // on은 이벤트를 걸었단 뜻
-                imgElem.src = reader.result;
+                reader.readAsDataURL(imgSource);
+                reader.onload = function() {
+                    imgElem.src = reader.result;
                 };
 
                 const shareBtnElem = body.querySelector('button');
@@ -39,8 +37,6 @@
                     const files = frmElem.imgs.files;
 
                     const fData = new FormData();
-                    // FormData(): form태그를 새로 만들어 주는 역할
-                    // ajax로 데이터를 보낼땐 formdata가 필요하다.
                     for(let i=0; i<files.length; i++) {
                         fData.append('imgs[]', files[i]);
                     }
@@ -49,32 +45,25 @@
 
                     fetch('/feed/rest', {
                         method: 'post',
-                        body: fData
-                        // fetch에서 데이터를 담는 body
+                        body: fData                       
                     }).then(res => res.json())
                         .then(myJson => {
-                            console.log(myJson);
+                           console.log(myJson);
 
-                            if(myJson.result) {
+                           if(myJson.result) {                                
                                 btnClose.click();
-                            }
-                            // const closeBtn = modal.querySelector('.btn-close');
-                            // closeBtn.click();
-
-                            // if(feedObj && myJson.result) {
-                            //     feedObj.refreshList();
-                            // }
+                           }
                         });
+                        
                 });
             }
         });
 
         btnNewFeedModal.addEventListener('click', function() {
             const selFromComBtn = document.createElement('button');
-            selFromComBtn.type = 'button'; 
-            // 기본 설정이 submit으로 돼있어서 새로 타입을 설정해 줌
+            selFromComBtn.type = 'button';
             selFromComBtn.className = 'btn btn-primary';
-            selFromComBtn.innerText = '컴퓨터에서 선택';
+            selFromComBtn.innerText = '컴퓨터에서 선택';            
             selFromComBtn.addEventListener('click', function() {
                 frmElem.imgs.click();
             });
@@ -82,4 +71,93 @@
             body.appendChild(selFromComBtn);
         });
     }
+
+    
+    const feedObj = {
+        limit: 20,
+        itemLength: 0,
+        currentPage: 1,
+        loadingElem: document.querySelector('.loading'),
+        containerElem: document.querySelector('#item_container'),
+        getFeedList: function() {
+            this.showLoading();            
+            const param = {
+                page: this.currentPage++
+            }
+            fetch('/feed/rest' + encodeQueryString(param))
+            .then(res => res.json())
+            .then(list => {                
+                this.makeFeedList(list);                
+            })
+            .catch(e => {
+                console.error(e);
+                this.hideLoading();
+            });
+        },
+        makeFeedList: function(list) {
+            if(list.length !== 0) {
+                list.forEach(item => {
+                    const divItem = this.makeFeedItem(item);
+                    this.containerElem.appendChild(divItem);
+                });
+            }
+            this.hideLoading();
+        },
+        makeFeedItem: function(item) {
+            console.log(item);
+            const divContainer = document.createElement('div');
+            divContainer.className = 'item mt-3 mb-3';
+            
+            const divTop = document.createElement('div');
+            divContainer.appendChild(divTop);
+
+            const regDtInfo = getDateTimeInfo(item.regdt);
+            divTop.className = 'd-flex flex-row ps-3 pe-3';
+            const writerImg = `<img src='/static/img/profile/${item.iuser}/${item.mainimg}' 
+                onerror='this.error=null;this.src="/static/img/profile/defaultProfileImg_100.png"'>`;
+
+            divTop.innerHTML = `
+                <div class="d-flex flex-column justify-content-center">
+                    <div class="circleimg h40 w40">${writerImg}</div>
+                </div>
+                <div class="p-3 flex-grow-1">
+                    <div><span class="pointer" onclick="moveToProfile(${item.iuser});">${item.writer}</span> - ${regDtInfo}</div>
+                    <div>${item.location === null ? '' : item.location}</div>
+                </div>
+            `;
+
+            const divImgSwiper = document.createElement('div');
+            divContainer.appendChild(divImgSwiper);
+            divImgSwiper.className = 'swiper item_img';
+            divImgSwiper.innerHTML = `
+                <div class="swiper-wrapper"></div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            `;
+            const divSwiperWrapper = divImgSwiper.querySelector('.swiper-wrapper');
+            
+            //TODO : imgList forEach 돌릴 예정
+            const imgObj = item.imgList[0];
+            const divSwiperSlide = document.createElement('div');
+            divSwiperWrapper.appendChild(divSwiperSlide);
+            divSwiperSlide.classList.add('swiper-slide');
+
+            const img = document.createElement('img');
+            divSwiperSlide.appendChild(img);
+            img.className = 'w614';
+            img.src = `/static/img/feed/${item.ifeed}/${imgObj.img}`;
+
+            return divContainer;
+        },
+
+        showLoading: function() { this.loadingElem.classList.remove('d-none'); },
+        hideLoading: function() { this.loadingElem.classList.add('d-none'); }
+
+    }
+    feedObj.getFeedList();
+
+
+
+
 })();
