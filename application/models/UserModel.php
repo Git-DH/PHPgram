033d298ad46f3e00e0bc092bba94ff82959eceda
Git-DH <kdh9345@naver.com>
2022-07-01 17:54:07 +0900

    07-01

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace application\models;
+
 use PDO;
 //$pdo -> lastInsertId();
 
-class UserModel extends Model {
-    public function insUser(&$param) {
+class UserModel extends Model
+{
+    public function insUser(&$param)
+    {
         $sql = "INSERT INTO t_user
                 ( email, pw, nm ) 
                 VALUES 
@@ -15,23 +19,27 @@ class UserModel extends Model {
         $stmt->bindValue(":nm", $param["nm"]);
         $stmt->execute();
         return $stmt->rowCount();
-
     }
-    public function selUser(&$param) {
+    public function selUser(&$param)
+    {
         $sql = "SELECT * FROM t_user
                 WHERE email = :email";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":email", $param["email"]);        
+        $stmt->bindValue(":email", $param["email"]);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
-    
-    public function selUserByIuser(&$param) {
-        $sql = "SELECT iuser, email, nm, cmt, mainimg, regdt 
+
+    public function selUserProfile(&$param)
+    {   $feediuser = $param["feediuser"];
+        $loginiuser = $param["loginiuser"];
+        $sql = "SELECT iuser, email, nm, cmt, mainimg
+                    , (SELECT COUNT(ifeed) FROM t_feed WHERE iuser = {$feediuser}) AS feedcnt
+                    , (SELECT COUNT(fromiuser) FROM t_user_follow WHERE fromiuser = {$feediuser} AND toiuser = {$loginiuser}) AS youme
+                    , (SELECT COUNT(toiuser) FROM t_user_follow WHERE fromiuser = {$loginiuser} AND toiuser = {$feediuser}) AS meyou
                 FROM t_user
-                WHERE iuser = :iuser";
+                WHERE iuser = {$feediuser}";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->bindValue(":iuser", $param["iuser"]);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
